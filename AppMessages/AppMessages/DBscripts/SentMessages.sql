d9baebc7f62f1ef02296bff4fe5d3e3e2b4589b2
rafaelnoyolal756 <rafaelnoyola@gmail.com>
2025-01-21 12:19:46 -0600

    Added scripts for tables in DB: Messages, SentMessages and TwilioSettings that keep settings for text messages with twilio service

---
+++ AppMessages/AppMessages/DBscripts/SentMessages.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[SentMessages](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MessageId] [int] NOT NULL,
+	[Sent] [datetime] NULL,
+	[ConfirmationCode] [nvarchar](80) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[SentMessages]  WITH CHECK ADD FOREIGN KEY([MessageId])
+REFERENCES [dbo].[Messages] ([Id])
+GO
